436acfbb6d08a2de005003631ab16d10911255a5
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-05-21 14:07:57 +0300

    Создайте ссылки на страницы в компоненте Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
+import {NavLink} from "react-router-dom";
+
 function Header() {
   return (
     <div>
-      <header>Header</header>
+      <header style={{padding: '1rem', background: '#eee'}}>
+          <nav style={{display: 'flex', gap: '1rem'}}>
+              <NavLink to='/'>Главная</NavLink>
+              <NavLink to='/about'>О нас</NavLink>
+              <NavLink to='/products'>Продукты</NavLink>
+              <NavLink to='/search'>Поиск</NavLink>
+              <NavLink to='/login'>Вход</NavLink>
+          </nav>
+      </header>
       <hr />
     </div>
   );
